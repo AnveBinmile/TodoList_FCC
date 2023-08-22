4ae8b17cde3b25e7b18d10453783d2f284a31526
anvesha <anvesha@binmile.com>
2023-08-22 12:08:38 +0530

    basic done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+let name:string;
+let age:number;
+let isStudent:boolean|string;
+let hobbies:number[];
+let role:[number,string];
+
+// let printName:Function;
+
+role=[5,'Anvesha'];
+
+
+
+type X={
+  a:string;
+  b:number;
+};
+
+type Y = X | {
+  c:string;
+  d:number;
+}
+
+let y: Y={
+  a:'Alice',
+  b:32,
+}
+
+interface Person extends X{
+  name:string;
+  age?:number; //Optional property
+}
+
+
+
+let person:Person={
+  name:'Anvesha',
+  age:22,
+  a:'Alice',
+  b:32,
+};
+
+
+let lotsOfPeople : Person[];
+
+let printName:(name:string)=>void=(name)=>{
+  console.log(name);
+  console.log(person);
+};
+
+// function printName(name:string){
+//     console.log(name);
+// }
+
+
+printName('Anvesha');
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
